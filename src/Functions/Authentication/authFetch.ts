a57de221a568b3d5cdc0f98898f5f1ee9e4f81f7
A-Jones343 <alexj.career@gmail.com>
2025-10-18 00:03:46 -0500

    Fixed Auth issues.

--- src/Functions/Authentication/authFetch.ts
+++ src/Functions/Authentication/authFetch.ts
@@ -2,45 +2,13 @@ import { fetch, FetchRequestInit } from "expo/fetch";
 import tokenHandler from "./tokenHandler";
 
 
-export const authFetchStatusCodes = {
-	OFFLINE: 19,
-	ABORTED: 20,
-	UNKNOWN_ERROR: 0
-}
-
-export default async function authFetch(url: string, options: FetchRequestInit) {
+export default async function authFetch(url: string, options: FetchRequestInit = {method: 'GET'}) {
 	const updatedHeader = new Headers(options.headers)
 	updatedHeader.append('Authorization', `Bearer ${await tokenHandler()}`)
 	options.headers = updatedHeader
 
-	try {
-		return await fetch(url, options)
-	}
-	catch (e: any) {
-		// Network related error code: 19
-		if(e instanceof TypeError) {
-			return new Response(null, {
-				status: authFetchStatusCodes.OFFLINE,
-				statusText: 'Network connection failed or server is unreachable.'
-			})
-		}
-
-		// Fetch aborted code: 20
-		if(e.name === 'AbortError') {
-			return new Response(null, {
-				status: authFetchStatusCodes.ABORTED,
-				statusText: 'Request was aborted by the client.'
-			})
-		}
-
-		// Other error code: 0
-		else {
-			return new Response(null, {
-				status: authFetchStatusCodes.UNKNOWN_ERROR,
-				statusText: `Unknown Fetch Error: ${e.message}`
-			})
-		}
-	}
-
-
+	return await fetch(url, options)
 }
+
+
+
--- src/Functions/login.ts
+++ src/Functions/login.ts
@@ -1,32 +1,41 @@
 import { api_root_url } from "../Settings/constants";
-import { setToken } from "./keyStore";
+import { setToken, getToken } from "./keyStore";
 
 export default async function login(email: string, password: string): 
 	Promise<{success: boolean, msg: string} | undefined> {
-	const endpoint:string = api_root_url + 'token/pair';
+	const endpoint: string = api_root_url + 'login';
 
+	let response: Response;
 	try {
-		const response: Response = await fetch(endpoint, {
+		response = await fetch(endpoint, {
 			method: "POST",
 			headers: {'Content-Type': 'application/json'},
 			body: JSON.stringify({
 				email: email,
 				password: password
 			})
-        });
+		});
+	} catch (e) {
+		return {success: false, msg: 'Try again later'}
+	}
 
-		const json = await response.json()
-		console.log(json)
-		// If user is unauthorized
-		if(response.status === 401) 
-			return {success: false, msg: 'The email or password entered is incorrect'}
+	const json = await response.json()
+	switch (response.status) {
+		case 400:
+			return {success: false, msg: 'The password entered is incorrect'}
 
-		// Saves access/refresh tokens necessary for authorization
+		case 404:
+			return {success: false, msg: 'An account with this email does not exist'}
 
-		await setToken({token: 'access'}, json.access)
-		await setToken({token: 'refresh'}, json.refresh)
+		case 422:
+			return {success: false, msg: 'The email entered is invalid'}
 
-    } catch (e) { console.log("Error: " + e); return {success: false, msg: 'Try again later'} }
+		case 200:
+			// Saves access/refresh tokens necessary for authorization
+			await setToken({token: 'access'}, json.access);
+			await setToken({token: 'refresh'}, json.refresh);
+			return {success: true, msg: 'success'}
 
-		return {success: true, msg: 'success'}
+		default: return {success: false, msg: 'Try again later'}
+	}
 }
