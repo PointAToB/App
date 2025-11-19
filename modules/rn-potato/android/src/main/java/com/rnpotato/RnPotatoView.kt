1c3299bb66e65a5699ae25010f675d7cd92ec0b3
A-Jones343 <alexj.career@gmail.com>
2025-11-19 00:21:52 -0600

    Figuring out preview issues with HAL

--- modules/rn-potato/android/src/main/java/com/rnpotato/RnPotatoView.kt
+++ modules/rn-potato/android/src/main/java/com/rnpotato/RnPotatoView.kt
@@ -2,14 +2,74 @@ package com.rnpotato
 
 import android.content.Context
 import android.util.AttributeSet
-import android.view.View
+import android.widget.FrameLayout
+import android.util.Log
+import com.rnpotato.databinding.CameraUiBinding
+import androidx.camera.lifecycle.ProcessCameraProvider
+import com.google.common.util.concurrent.ListenableFuture
+import androidx.camera.core.Preview
+import androidx.camera.core.CameraSelector
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.LifecycleRegistry
+import android.view.LayoutInflater
+import java.util.concurrent.TimeUnit
 
-class RnPotatoView : View {
-  constructor(context: Context?) : super(context)
-  constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs)
-  constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) : super(
-    context,
-    attrs,
-    defStyleAttr
-  )
+
+class RnPotatoView : FrameLayout, LifecycleOwner {
+  constructor(context: Context) : super(context) { configureComponent() }
+  constructor(context: Context, attrs: AttributeSet?) : super(context, attrs) { configureComponent() }
+  constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr) { configureComponent() }
+
+  private lateinit var cameraProviderFuture : ListenableFuture<ProcessCameraProvider>
+  private val lifecycleRegistry = LifecycleRegistry(this)
+  private lateinit var binding : CameraUiBinding
+  private var isMounted : Boolean = false
+  private fun configureComponent() {
+    Log.e("Watermelon", "Started")
+    cameraProviderFuture = ProcessCameraProvider.getInstance(context)
+    binding = CameraUiBinding.inflate(LayoutInflater.from(context), this, true)
+  }
+
+  override fun onAttachedToWindow() {
+    super.onAttachedToWindow()
+
+    // If the component has already been mounted, prevents mounting again, unless new instance.
+    if (isMounted) return
+
+    isMounted = true
+    start()
+    camera({provider ->
+      var preview : Preview = Preview.Builder().build()
+
+      var cameraSelector : CameraSelector = CameraSelector.Builder().requireLensFacing(CameraSelector.LENS_FACING_FRONT).build()
+
+      preview.setSurfaceProvider(binding.previewView.getSurfaceProvider())
+
+      var camera = provider.bindToLifecycle(this, cameraSelector, preview)
+    })
+  }
+
+  override fun onDetachedFromWindow() {
+    super.onDetachedFromWindow()
+
+    isMounted = false
+    destroy()
+  }
+
+  private fun camera(func: (provider: ProcessCameraProvider) -> Unit) {
+    cameraProviderFuture.addListener(Runnable {
+      val provider = cameraProviderFuture.get(5, TimeUnit.SECONDS)
+      func(provider)
+    }, ContextCompat.getMainExecutor(context))
+  }
+
+  fun start() { lifecycleRegistry.handleLifecycleEvent(Lifecycle.Event.ON_START) }
+  fun stop() { lifecycleRegistry.handleLifecycleEvent(Lifecycle.Event.ON_STOP) }
+  fun destroy() { lifecycleRegistry.handleLifecycleEvent(Lifecycle.Event.ON_DESTROY) }
+
+  override val lifecycle: Lifecycle
+    get() = lifecycleRegistry
 }
+
--- src/Pages/camera.tsx
+++ src/Pages/camera.tsx
@@ -9,7 +9,7 @@ import Close from "../Components/Camera/close";
 import { CameraType } from "expo-camera";
 import Selector from "../Components/Camera/selector";
 import { selectable, photo, video, coach } from '../Components/Camera/selectable'
-
+import { RnPotatoView } from 'rn-potato'
 
 const options: selectable[] = [video, photo, coach]
 
@@ -21,7 +21,7 @@ export function Camera (props: {navigation: NativeStackNavigationProp<any>}) {
 	return (
 		<View style={styles.main}>
 			<View style={styles.camera}>
-
+                <RnPotatoView style={{flex: 1}}/>
 			</View>
 
 			<View style={styles.menu}>
